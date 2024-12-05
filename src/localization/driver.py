6b1a82d3b12ca61ab322ac2577ef7e4bdc111802
Conor Gagliardi <112734764+ConorGagliardi@users.noreply.github.com>
2024-12-05 15:34:25 -0800

    Update driver.py
    added some functionality, just need go to goal function fleshed out

--- src/localization/driver.py
+++ src/localization/driver.py
@@ -16,6 +16,7 @@ class Driver(Node):
     def __init__(self):
         super().__init__('driver') # type: ignore
         self.point = None
+        self.current_point = None
 
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
         self.goal_subscription = self.create_subscription(
@@ -34,11 +35,18 @@ class Driver(Node):
     
     def position_callback(self, msg):
         # Adjust the proportional drive with every callback
-        pass
+        self.current_position = msg
+
+        if(self.position is not None):
+            # go to goal
 
     def goal_callback(self, msg):
-        # set a point and rememeber it? Then repeat after some amount of time has elapsed?
-        pass
+        # set a point and remember it? Then repeat after some amount of time has elapsed?
+        self.point = msg
+
+    def go_to_light(self):
+        #take in positional data and calculate angle and distance. Send twist commands until
+        #both are zero or under a given margin
 
 if __name__ == "__main__":
 
